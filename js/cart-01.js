ca765372c2fa538a3cfc2ddbb945664cc5a5958f
islam <ibiigaziev@gmail.com>
2023-02-20 23:13:40 +0600

    finish commit

---
+++ js/cart-01.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const cartWrapper = document.querySelector('.cart-wrapper');
+
+window.addEventListener('click', e => {
+  if (e.target.hasAttribute('data-cart')) {
+    const card = e.target.closest('.card')
+  
+
+  const productInfo = {
+    id: card.dataset.id,
+    imgSrc: card.querySelector('.product-img').getAttribute('src'),
+    title: card.querySelector('.item-title').innerText,
+    itemsInBox: card.querySelector('[data-items-in-box]').innerText,
+    weight: card.querySelector('.price__weight').innerText,
+    price: card.querySelector('.price__currency').innerText,
+    counter: card.querySelector('[data-counter]').innerText
+  };
+  console.log(productInfo)
+
+  const cartItemHTML = `<div class="cart-item" data-id="${productInfo.id}">
+  <div class="cart-item__top">
+    <div class="cart-item__img">
+      <img src="${productInfo.imgSrc}" alt="">
+    </div>
+    <div class="cart-item__desc">
+      <div class="cart-item__title">${productInfo.title}</div>
+      <div class="cart-item__weight">${productInfo.itemsInBox} / ${productInfo.weight}</div>
+
+      <div class="cart-item__details">
+
+        <div class="items items--small counter-wrapper">
+          <div class="items__control" data-action="minus">-</div>
+          <div class="items__current" data-counter="">${productInfo.counter}</div>
+          <div class="items__control" data-action="plus">+</div>
+        </div>
+
+        <div class="price">
+          <div class="price__currency">${productInfo.price}</div>
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+</div>`;
+
+  cartWrapper.insertAdjacentHTML('beforeend', cartItemHTML)
+}
+  });
